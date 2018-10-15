020d931a264c89cefadfcf6a6dd6003f63bfed52
valentinus silalahi <valentinus.silalahi@gmail.com>
2018-10-15 16:38:06 +0700

    1. fisrt commit add dependency, create skema db awal and README

---
+++ java-backend/src/main/java/com/silalahi/valentinus/fintech/MicrofinanceCrowdfundingApplication.java
@@ -0,0 +1,12 @@
+package com.silalahi.valentinus.fintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrofinanceCrowdfundingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrofinanceCrowdfundingApplication.class, args);
+	}
+}
---
+++ java-backend/src/main/resources/db/migration/V1.0.0.2018101501__Skema_Awal.sql
@@ -0,0 +1,6 @@
+CREATE TABLE application_info (
+	id VARCHAR(36),
+	name VARCHAR(255) NOT NULL,
+	value VARCHAR(100) NOT NULL,
+	PRIMARY KEY (id)
+);
